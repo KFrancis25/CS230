0ede1dbe66a85ee86e3b3cc6c96466b162cfe6a1
Drew Guarnera <ryonek25@wooster.edu>
2024-09-02 12:28:53 -0400

    Dog Methods Added

--- Lab02.py
+++ Lab02.py
@@ -32,5 +32,42 @@ class TestBasketball(unittest.TestCase):
         game = Basketball("Basketball", 5)
         self.assertEqual(game.team_size, 5)
 
+# Author: Ryan Yonek
+# Date: 9/2/24
+# Adding two methods and unit tests to exisiting project
+# 
+
+
+# Import unit test
+import unittest
+
+# Base class
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def get_love(self):
+        return "Is a good girl"
+    
+# Derived class
+class Dori(Dog):
+    def __init__(self, name, numSit):
+        super().__init__(name)
+        self.numSit = numSit
+
+    def get_love(self):
+        return "Is a good girl"
+
+# Unit Tests
+class TestDori(unittest.TestCase):
+
+    def test_get_love(self):
+        goodGirl = Dori("Is a good girl", 10)
+        self.assertEqual(goodGirl.get_love(), "Is a good girl")
+
+    def test_numSit(self):
+        goodGirl = Dori("Is a good girl", 10)
+        self.assertEqual(goodGirl.numSit, 10)
+
 if __name__ == "__main__":
     unittest.main()
