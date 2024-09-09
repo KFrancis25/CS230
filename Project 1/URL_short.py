db2cefc7c8ffd6a8d32a270fecabd0fb74d7ca1a
Kai <kfrancis25@wooster.edu>
2024-09-09 12:21:14 -0400

    project 1

---
+++ Project 1/URL_short.py
@@ -0,0 +1,8 @@
+import random
+import string
+import json
+import re
+
+#store data mappings
+URL_DATA_FILE = "url_data.json"
+
--- Ryan_Lab02.py
+++ Ryan_Lab02.py
@@ -26,27 +26,10 @@ class Basketball(Sport):
         return "5 people"
 
     def get_game_duration(self):
-        return "48 mintues" #standard NBA game length
+        return "40 mintues" #standard NBA game length
 
     def get_court_size(self):
         return "94 feet long, 50 feet wide" #NBA court size
-    
-class Tennis(Sport):
-    def __init__(self, name, team_size):
-        super().__init__(name)
-        self.team_size = team_size
-
-    def get_equipment(self):
-        return "Tennis"
-
-    def team_size(self):
-        return "2"
-
-    def get_game_duration(self):
-        return "30 to 60 minutes" 
-
-    def get_court_size(self):
-        return "78ft long by 36ft wide"
 
 
 class TestBasketball(unittest.TestCase):
@@ -60,30 +43,17 @@ class TestBasketball(unittest.TestCase):
         self.assertEqual(game.team_size, 5)
 
     def test_get_game_duration(self):
-        game = Basketball("Basketball", 5)
-        self.assertEqual(game.get_game_duration(), "48 mintues")
+        game = Basketball("Basketball", 5, "40 minutes")
+        self.assertEqual(game.get_game_duration(), "40 minutes")
 
     def test_get_court_size(self):
         game = Basketball("Basketball", 5)
         self.assertEqual(game.get_court_size(), "94 feet long, 50 feet wide")
-
-class TestTennis(unittest.TestCase):
-     def test_get_equipment(self):
-        game = Tennis("Tennis ", 2)
-        self.assertEqual(game.get_equipment(), "Tennis")
-
-     def test_team_size(self):
-        game = Tennis("Tennis", 2)
-        self.assertEqual(game.team_size, 2)
-
-     def test_get_game_duration(self):
-        game = Tennis("Tennis", 2)
-        self.assertEqual(game.get_game_duration(), "30 to 60 minutes")
-
-     def test_get_court_size(self):
-        game = Tennis("Tennis", 2)
-        self.assertEqual(game.get_court_size(), "78ft long by 36ft wide")
     
     
 if __name__ == "__main__":
     unittest.main()
+
+
+
+        
