6361eaedd9ac7c69d714b65ea12c0ec9dd8547ff
Kai <kfrancis25@wooster.edu>
2024-09-09 12:26:46 -0400

    project 1 update

--- Project 1/URL_short.py
+++ Project 1/URL_short.py
@@ -6,3 +6,15 @@ import re
 #store data mappings
 URL_DATA_FILE = "url_data.json"
 
+#load data from JSON file
+def load_url_data():
+    try:
+        with open(URL_DATA_FILE, 'r') as file: 
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:
+        return {}
+    
+
+#Save url data
