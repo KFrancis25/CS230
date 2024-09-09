90cb906eb9923228b76d5b45e30c1f669e378966
Kai <kfrancis25@wooster.edu>
2024-09-09 12:43:17 -0400

    project update

--- Project 1/URL_short.py
+++ Project 1/URL_short.py
@@ -35,4 +35,24 @@ def validate_url(url):
     return url_pattern.match(url) is not None
 
 #shorten the URL
+def shorten_url(long_url):
+    if not validate_url(long_url):
+        return "Invalid URL. Please enter a valid URL."
+    
+    #load URL data
+    url_data = load_url_data()
 
+    #Check to see if the URL already exists
+    for short_id, stored_url in url_data.items():
+        if stored_url == long_url:
+            return f"Shortened URL already exists{short_id}"
+
+    #generate new ID    
+    short_id = generate_short_id()
+    while short_id in url_data:
+        short_id = generate_short_id()
+
+    # Store the new URL
+    url_data[short_id] = long_url
+    save_url_data(url_data)
+    return f"Shortened URL: {short_id}"
