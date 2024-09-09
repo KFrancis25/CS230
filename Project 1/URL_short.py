e588954dc96bab9128447003afd656a0341e5e55
Kai <kfrancis25@wooster.edu>
2024-09-09 12:35:25 -0400

    project update

--- Project 1/URL_short.py
+++ Project 1/URL_short.py
@@ -25,3 +25,14 @@ def save_url_data():
 #generate random ID
 def generate_random_id():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+
+#Validate URL format
+def validate_url(url):
+    url_pattern = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:[\w-]+\.)+[a-z]{2,6}'  # domain
+        r'(?:[/?#][^\s]*)?$', re.IGNORECASE)
+    return url_pattern.match(url) is not None
+
+#shorten the URL
+
