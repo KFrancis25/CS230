f170a3a162b2f2d48965f8cf679771f3ccc2c6f5
Kai <kfrancis25@wooster.edu>
2024-09-09 12:29:06 -0400

    project 1

--- Project 1/URL_short.py
+++ Project 1/URL_short.py
@@ -18,3 +18,10 @@ def load_url_data():
     
 
 #Save url data
+def save_url_data():
+    with open(URL_DATA_FILE, 'w') as file:
+        json.dump(data, file)
+
+#generate random ID
+def generate_random_id():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
